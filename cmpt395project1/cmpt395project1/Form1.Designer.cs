f4fd585e8b0b03d6e0170f4f42c0cbb95b28e920
andre <adillea2@mymacewan.ca>
2026-05-24 18:55:47 -0600

    full commit

---
+++ cmpt395project1/cmpt395project1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace cmpt395project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnClearCart = new System.Windows.Forms.Button();
+            this.dgvCart = new System.Windows.Forms.DataGridView();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.dgvCourses = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cmbYear = new System.Windows.Forms.ComboBox();
+            this.cmbTerm = new System.Windows.Forms.ComboBox();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblTerm = new System.Windows.Forms.Label();
+            this.lblStudentID = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dgvEnrolled = new System.Windows.Forms.DataGridView();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtEnrollStudentID = new System.Windows.Forms.TextBox();
+            this.lblEnrollStudentID = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCourses)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEnrolled)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnClearCart);
+            this.tabPage1.Controls.Add(this.dgvCart);
+            this.tabPage1.Controls.Add(this.btnAddToCart);
+            this.tabPage1.Controls.Add(this.dgvCourses);
+            this.tabPage1.Controls.Add(this.btnSearch);
+            this.tabPage1.Controls.Add(this.cmbYear);
+            this.tabPage1.Controls.Add(this.cmbTerm);
+            this.tabPage1.Controls.Add(this.txtStudentID);
+            this.tabPage1.Controls.Add(this.lblYear);
+            this.tabPage1.Controls.Add(this.lblTerm);
+            this.tabPage1.Controls.Add(this.lblStudentID);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Shopping Cart";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // btnClearCart
+            // 
+            this.btnClearCart.Location = new System.Drawing.Point(40, 392);
+            this.btnClearCart.Name = "btnClearCart";
+            this.btnClearCart.Size = new System.Drawing.Size(75, 23);
+            this.btnClearCart.TabIndex = 11;
+            this.btnClearCart.Text = "Clear Cart";
+            this.btnClearCart.UseVisualStyleBackColor = true;
+            this.btnClearCart.Click += new System.EventHandler(this.btnClearCart_Click);
+            // 
+            // dgvCart
+            // 
+            this.dgvCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCart.Location = new System.Drawing.Point(40, 280);
+            this.dgvCart.MultiSelect = false;
+            this.dgvCart.Name = "dgvCart";
+            this.dgvCart.ReadOnly = true;
+            this.dgvCart.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCart.Size = new System.Drawing.Size(648, 104);
+            this.dgvCart.TabIndex = 10;
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.Location = new System.Drawing.Point(40, 248);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(75, 23);
+            this.btnAddToCart.TabIndex = 9;
+            this.btnAddToCart.Text = "Add to Cart";
+            this.btnAddToCart.UseVisualStyleBackColor = true;
+            this.btnAddToCart.Click += new System.EventHandler(this.btnAddToCart_Click);
+            // 
+            // dgvCourses
+            // 
+            this.dgvCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCourses.Location = new System.Drawing.Point(40, 120);
+            this.dgvCourses.MultiSelect = false;
+            this.dgvCourses.Name = "dgvCourses";
+            this.dgvCourses.ReadOnly = true;
+            this.dgvCourses.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCourses.Size = new System.Drawing.Size(653, 96);
+            this.dgvCourses.TabIndex = 8;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(40, 80);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(103, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search Courses";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click_1);
+            // 
+            // cmbYear
+            // 
+            this.cmbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbYear.FormattingEnabled = true;
+            this.cmbYear.Location = new System.Drawing.Point(520, 31);
+            this.cmbYear.Name = "cmbYear";
+            this.cmbYear.Size = new System.Drawing.Size(121, 21);
+            this.cmbYear.TabIndex = 5;
+            // 
+            // cmbTerm
+            // 
+            this.cmbTerm.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTerm.FormattingEnabled = true;
+            this.cmbTerm.Location = new System.Drawing.Point(260, 31);
+            this.cmbTerm.Name = "cmbTerm";
+            this.cmbTerm.Size = new System.Drawing.Size(121, 21);
+            this.cmbTerm.TabIndex = 4;
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(40, 32);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(100, 20);
+            this.txtStudentID.TabIndex = 3;
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(560, 15);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(32, 13);
+            this.lblYear.TabIndex = 2;
+            this.lblYear.Text = "Year:";
+            // 
+            // lblTerm
+            // 
+            this.lblTerm.AutoSize = true;
+            this.lblTerm.Location = new System.Drawing.Point(298, 15);
+            this.lblTerm.Name = "lblTerm";
+            this.lblTerm.Size = new System.Drawing.Size(34, 13);
+            this.lblTerm.TabIndex = 1;
+            this.lblTerm.Text = "Term:";
+            // 
+            // lblStudentID
+            // 
+            this.lblStudentID.AutoSize = true;
+            this.lblStudentID.Location = new System.Drawing.Point(56, 16);
+            this.lblStudentID.Name = "lblStudentID";
+            this.lblStudentID.Size = new System.Drawing.Size(67, 13);
+            this.lblStudentID.TabIndex = 0;
+            this.lblStudentID.Text = "Studenbt ID:";
+            this.lblStudentID.Click += new System.EventHandler(this.lblStudentID_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dgvEnrolled);
+            this.tabPage2.Controls.Add(this.btnRegister);
+            this.tabPage2.Controls.Add(this.txtEnrollStudentID);
+            this.tabPage2.Controls.Add(this.lblEnrollStudentID);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Enroll";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dgvEnrolled
+            // 
+            this.dgvEnrolled.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEnrolled.Location = new System.Drawing.Point(41, 137);
+            this.dgvEnrolled.MultiSelect = false;
+            this.dgvEnrolled.Name = "dgvEnrolled";
+            this.dgvEnrolled.ReadOnly = true;
+            this.dgvEnrolled.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvEnrolled.Size = new System.Drawing.Size(531, 174);
+            this.dgvEnrolled.TabIndex = 3;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(48, 352);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(127, 23);
+            this.btnRegister.TabIndex = 2;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // txtEnrollStudentID
+            // 
+            this.txtEnrollStudentID.Location = new System.Drawing.Point(124, 48);
+            this.txtEnrollStudentID.Name = "txtEnrollStudentID";
+            this.txtEnrollStudentID.Size = new System.Drawing.Size(100, 20);
+            this.txtEnrollStudentID.TabIndex = 1;
+            // 
+            // lblEnrollStudentID
+            // 
+            this.lblEnrollStudentID.AutoSize = true;
+            this.lblEnrollStudentID.Location = new System.Drawing.Point(47, 51);
+            this.lblEnrollStudentID.Name = "lblEnrollStudentID";
+            this.lblEnrollStudentID.Size = new System.Drawing.Size(61, 13);
+            this.lblEnrollStudentID.TabIndex = 0;
+            this.lblEnrollStudentID.Text = "Student ID:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCourses)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEnrolled)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dgvCourses;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cmbYear;
+        private System.Windows.Forms.ComboBox cmbTerm;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblTerm;
+        private System.Windows.Forms.Label lblStudentID;
+        private System.Windows.Forms.DataGridView dgvEnrolled;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtEnrollStudentID;
+        private System.Windows.Forms.Label lblEnrollStudentID;
+        private System.Windows.Forms.Button btnClearCart;
+        private System.Windows.Forms.DataGridView dgvCart;
+        private System.Windows.Forms.Button btnAddToCart;
+    }
+}
+
---
+++ cmpt395project1/cmpt395project1/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;  
+
+namespace cmpt395project1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection myConnection;
+        SqlCommand myCommand;
+        SqlDataReader myReader;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                myConnection = new SqlConnection(
+                    "Server=LAPTOP-3MSVSB2A;" +
+                    "Database=Academic_Department_Course;" +
+                    "Integrated Security=True;"
+                );
+                myConnection.Open();
+                myCommand = new SqlCommand();
+                myCommand.Connection = myConnection;
+            }
+            catch
+            {
+            }
+            
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // accidental copy of the search code, will be removed in final version
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            // accidental copy of the register code, will be removed in final version
+        }
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStudentID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // loads term and year dropdowns from database, setsup cart columns
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            myCommand.CommandText = "SELECT DISTINCT Term FROM Course_sec";
+            myReader = myCommand.ExecuteReader();
+            while (myReader.Read())
+            {
+                cmbTerm.Items.Add(myReader["Term"].ToString());
+            }
+            myReader.Close();
+            cmbTerm.SelectedIndex = 0;
+
+            myCommand.CommandText = "SELECT DISTINCT Year FROM Course_sec ORDER BY Year";
+            myReader = myCommand.ExecuteReader();
+            while (myReader.Read())
+            {
+                cmbYear.Items.Add(myReader["Year"].ToString());
+            }
+            myReader.Close();
+            cmbYear.SelectedIndex = 0;
+
+            // setup cart datagridview column, enrollment will be done based on SectionID, other columns are just for display purposes
+            dgvCart.Columns.Add("StudentID", "Student ID");
+            dgvCart.Columns.Add("SectionID", "Section ID");
+            dgvCart.Columns.Add("CourseCode", "Course Code");
+            dgvCart.Columns.Add("CourseName", "Course Name");
+            dgvCart.Columns.Add("Instructor", "Instructor");
+            dgvCart.Columns.Add("Day", "Day");
+            dgvCart.Columns.Add("StartTime", "Start Time");
+            dgvCart.Columns.Add("EndTime", "End Time");
+        }
+
+        // main search function, queries database for courses matching selected term and year, displays results in datagridview
+        private void btnSearch_Click_1(object sender, EventArgs e)
+        {
+            dgvCourses.Rows.Clear();
+            dgvCourses.Columns.Clear();
+
+            dgvCourses.Columns.Add("SectionID", "Section ID");
+            dgvCourses.Columns.Add("CourseCode", "Course Code");
+            dgvCourses.Columns.Add("CourseName", "Course Name");
+            dgvCourses.Columns.Add("Instructor", "Instructor");
+            dgvCourses.Columns.Add("Day", "Day");
+            dgvCourses.Columns.Add("StartTime", "Start Time");
+            dgvCourses.Columns.Add("EndTime", "End Time");
+            dgvCourses.Columns.Add("Capacity", "Capacity");
+            dgvCourses.Columns.Add("Seats Available", "Seats Available");
+
+            myCommand.CommandText =
+                "SELECT cs.SectionID, c.CourseCode, c.CourseName, " +
+                "i.FirstName + ' ' + i.LastName AS Instructor, " +
+                "cs.ScheduleDay, cs.StartTime, cs.EndTime, cs.Capacity, " +
+                "cs.Capacity - COUNT(r.RegistrationID) AS SeatsAvailable " +
+                "FROM Course_sec cs " +
+                "JOIN Course c ON cs.CourseID = c.CourseID " +
+                "JOIN Instructor i ON cs.InstructorID = i.InstructorID " +
+                "LEFT JOIN Registration r ON cs.SectionID = r.SectionID " +
+                "AND r.Status = 'Registered' " +
+                "WHERE cs.Term = @Term AND cs.Year = @Year " +
+                "GROUP BY cs.SectionID, c.CourseCode, c.CourseName, " +
+                "i.FirstName, i.LastName, cs.ScheduleDay, " +
+                "cs.StartTime, cs.EndTime, cs.Capacity";
+
+            myCommand.Parameters.Clear();
+            myCommand.Parameters.AddWithValue("@Term", cmbTerm.SelectedItem.ToString());
+            myCommand.Parameters.AddWithValue("@Year", int.Parse(cmbYear.SelectedItem.ToString()));
+
+            try
+            {
+                myReader = myCommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    dgvCourses.Rows.Add(
+                        myReader["SectionID"].ToString(),
+                        myReader["CourseCode"].ToString(),
+                        myReader["CourseName"].ToString(),
+                        myReader["Instructor"].ToString(),
+                        myReader["ScheduleDay"].ToString(),
+                        myReader["StartTime"].ToString(),
+                        myReader["EndTime"].ToString(),
+                        myReader["Capacity"].ToString(),
+                        myReader["SeatsAvailable"].ToString()
+                    );
+                }
+                myReader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+
+        // adds selected course from search results to cart datagridview, checks for duplicates
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+
+            // Check student ID is entered before adding to cart
+            if (txtStudentID.Text == "")
+            {
+                MessageBox.Show("Please enter your Student ID before adding to cart.", "Error");
+                return;
+            }
+
+            if (dgvCourses.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a course to add to cart.", "Error");
+                return;
+            }
+
+            DataGridViewRow selectedRow = dgvCourses.SelectedRows[0];
+            string sectionID = selectedRow.Cells["SectionID"].Value.ToString();
+
+            // Check if already in cart
+            foreach (DataGridViewRow row in dgvCart.Rows)
+            {
+                if (row.Cells["SectionID"].Value != null &&
+                    row.Cells["SectionID"].Value.ToString() == sectionID)
+                {
+                    MessageBox.Show("This course is already in your cart.", "Warning");
+                    return;
+                }
+            }
+
+            dgvCart.Rows.Add(
+                txtStudentID.Text,
+                sectionID,
+                selectedRow.Cells["CourseCode"].Value.ToString(),
+                selectedRow.Cells["CourseName"].Value.ToString(),
+                selectedRow.Cells["Instructor"].Value.ToString(),
+                selectedRow.Cells["Day"].Value.ToString(),
+                selectedRow.Cells["StartTime"].Value.ToString(),
+                selectedRow.Cells["EndTime"].Value.ToString()
+            );
+
+            MessageBox.Show("Course added to cart!", "Success");
+        }
+
+        // clears all courses from cart datagridview
+        private void btnClearCart_Click(object sender, EventArgs e)
+        {
+            dgvCart.Rows.Clear();
+            MessageBox.Show("Cart cleared!", "Success");
+        }
+    }
+}
+
+
+/*
+ * ENROLL TAB NOTES:
+ * - Cart data is in dgvCart (in memory)
+ * - Each row has StudentID column - filter by this to get the right student's courses
+ * - Get student ID from txtEnrollStudentID.Text on Enroll tab
+ * - Loop through dgvCart rows where StudentID matches, call RegisterStudent for each
+ * - Database connection already set up: use myConnection and myCommand
+ * - Stored procedure: EXEC RegisterStudent @StudentID, @SectionID, @RegistrationDate
+ */
